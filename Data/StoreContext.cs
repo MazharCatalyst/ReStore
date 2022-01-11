e1908c1f73b36cf61cec34e3a8e718b221876dac
Admin <Admin@DESIGNER-PC>
2022-01-11 09:22:55 -0500

    Add project files.

---
+++ Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
